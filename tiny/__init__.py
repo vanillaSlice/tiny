fd0089d3576cf889e43fee6d22bca8a263756c3e
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-11-05 11:37:58 +0000

    Updated env variable settings

--- tiny/__init__.py
+++ tiny/__init__.py
@@ -5,24 +5,31 @@ from mongoengine import connect
 # create app instance
 APP = Flask(__name__, instance_relative_config=True)
 
-# load default config
-APP.config.from_object("config")
+# # load default config
+# APP.config.from_object("config")
 
-# load instance config from environment variable or instance config if not set
-APP.config.from_pyfile(os.environ.get("TINY_SETTINGS") or "config.py", silent=True)
+# # load instance config from environment variable or instance config if not set
+# APP.config.from_pyfile(os.environ.get("TINY_SETTINGS") or "config.py", silent=True)
+
+APP.secret_key = os.environ.get("SECRET_KEY", "development key")
+db_name = os.environ.get("MONGODB_DB", "tiny")
+host = os.environ.get("MONGODB_HOST", "localhost")
+port = os.environ.get("MONGODB_PORT", 27017)
+username = os.environ.get("MONGODB_USERNAME", "")
+password = os.environ.get("MONGODB_PASSWORD", "")
 
 # connect to database (authenticating if necessary)
-if APP.config["MONGODB_USERNAME"] or APP.config["MONGODB_PASSWORD"]:
-    CONNECTION = connect(APP.config["MONGODB_DB"],
-                         host=APP.config["MONGODB_HOST"],
-                         port=APP.config["MONGODB_PORT"],
-                         username=APP.config["MONGODB_USERNAME"],
-                         password=APP.config["MONGODB_PASSWORD"])
+if username or password:
+    CONNECTION = connect(db_name,
+                         host=host,
+                         port=port,
+                         username=username,
+                         password=password)
 else:
-    CONNECTION = connect(APP.config["MONGODB_DB"],
-                         host=APP.config["MONGODB_HOST"],
-                         port=APP.config["MONGODB_PORT"])
+    CONNECTION = connect(db_name,
+                         host=host,
+                         port=port)
 
-DB = CONNECTION[APP.config["MONGODB_DB"]]
+DB = CONNECTION[db_name]
 
 from tiny import views
