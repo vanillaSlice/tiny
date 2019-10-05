45ba36c2f765880cc177ccb1a7a15376b9d1ef71
Mike Lowe <mikelowedev@gmail.com>
2019-10-05 12:16:05 +0100

    Release v1.4.1 (#27)

--- tiny/__init__.py
+++ tiny/__init__.py
@@ -10,7 +10,8 @@ from flask_mongoengine import MongoEngine
 
 from tiny.assets import bundles
 from tiny.helpers import markdown_to_html
-from tiny.version import __version__
+
+version = 'v1.4.1'
 
 assets = Environment()
 
@@ -52,10 +53,10 @@ def create_app(testing=False):
             os.environ.get('WTF_CSRF_ENABLED', str(app.config.get('WTF_CSRF_ENABLED'))).lower() == 'true'
     })
 
-    # set version
-    app.config.update({
-        'VERSION': __version__
-    })
+    # inject version
+    @app.context_processor
+    def inject_version():
+        return {'version': version}
 
     # init extensions
     assets.init_app(app)
--- tiny/version.py
+++
@@ -1,5 +0,0 @@
-"""
-Exports Tiny app version.
-"""
-
-__version__ = '1.4.0'
